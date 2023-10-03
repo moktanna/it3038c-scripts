bff0d8e67e07c51bd92af810538786f7830bed3f
moktanna <142691046+moktanna@users.noreply.github.com>
2023-10-02 23:28:43 -0400

    Update Lab5.py

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -1,1 +1,20 @@
+from datetime import datetime
+birth_date_str = input("Enter your birthdate (YYYY-MM-DD): ")
 
+try:
+
+    birth_date = datetime.strptime(birth_date_str, '%Y-%m-%d')
+
+
+    current_date = datetime.now()
+
+    
+    time_difference = current_date - birth_date
+    total_seconds = time_difference.total_seconds()
+
+    
+    print(f'You have lived for approximately {int(total_seconds)} seconds.')
+
+except ValueError:
+
+  print('Invalid date format. Please enter the date in proper format.')
